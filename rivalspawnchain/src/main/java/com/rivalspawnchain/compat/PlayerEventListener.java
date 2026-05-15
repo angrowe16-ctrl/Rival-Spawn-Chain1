2c6ffd006c990c3c2230b58667c13c2d54be67c5
angrowe16-ctrl <angrowe16@gmail.com>
2026-05-14 20:53:06 -0400

    Update PlayerEventListener.java

--- rivalspawnchain/src/main/java/com/rivalspawnchain/compat/PlayerEventListener.java
+++ rivalspawnchain/src/main/java/com/rivalspawnchain/compat/PlayerEventListener.java
@@ -5,7 +5,6 @@ import com.rivalspawnchain.chain.ChainManager;
 import com.rivalspawnchain.chain.PlayerChainData;
 import com.rivalspawnchain.network.NetworkHandler;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 import java.util.UUID;
@@ -15,18 +14,14 @@ public class PlayerEventListener {
     public static void register() {
 
         CobblemonEvents.BATTLE_VICTORY.subscribe(event -> {
-            var battle = event.getBattle();
-            MinecraftServer server = battle.getServer() instanceof MinecraftServer s ? s : null;
-
             event.getWinners().forEach(actor ->
                 actor.getPlayerUUIDs().forEach(uid -> {
                     event.getLosers().forEach(loser ->
                         loser.getPokemonList().forEach(bp -> {
                             String species = bp.getOriginalPokemon()
-                                    .getSpecies().name().toLowerCase();
+                                    .getSpecies().getName().toLowerCase();
                             PlayerChainData data = ChainManager.INSTANCE.get(uid);
                             data.recordKo(species);
-                            syncPlayer(server, uid, data);
                         })
                     );
                 })
@@ -34,29 +29,17 @@ public class PlayerEventListener {
             ChainManager.INSTANCE.save();
         });
 
-        CobblemonEvents.BATTLE_FLED.subscribe(event -> {
-            var battle = event.getBattle();
-            MinecraftServer server = battle.getServer() instanceof MinecraftServer s ? s : null;
-
+        CobblemonEvents.BATTLE_FLED.subscribe(event ->
             event.getBattle().getActors().forEach(actor ->
                 actor.getPlayerUUIDs().forEach(uid -> {
-                    PlayerChainData data = ChainManager.INSTANCE.get(uid);
-                    data.breakChain();
-                    syncPlayer(server, uid, data);
+                    ChainManager.INSTANCE.get(uid).breakChain();
                 })
-            );
-            ChainManager.INSTANCE.save();
-        });
+            )
+        );
 
         ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
             ServerPlayerEntity player = handler.getPlayer();
             NetworkHandler.sendToPlayer(player, ChainManager.INSTANCE.get(player.getUuid()));
         });
-    }
-
-    private static void syncPlayer(MinecraftServer server, UUID uid, PlayerChainData data) {
-        if (server == null) return;
-        ServerPlayerEntity player = server.getPlayerManager().getPlayer(uid);
-        if (player != null) NetworkHandler.sendToPlayer(player, data);
     }
 }
