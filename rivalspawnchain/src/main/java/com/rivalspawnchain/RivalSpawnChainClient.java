d7a3089ea35c64044005b0e078e9b17c5aab72fc
angrowe16-ctrl <angrowe16@gmail.com>
2026-05-14 20:56:32 -0400

    Update RivalSpawnChainClient.java

--- rivalspawnchain/src/main/java/com/rivalspawnchain/RivalSpawnChainClient.java
+++ rivalspawnchain/src/main/java/com/rivalspawnchain/RivalSpawnChainClient.java
@@ -1,19 +1,15 @@
 package com.rivalspawnchain;
 
 import com.rivalspawnchain.network.ClientNetworkHandler;
-import com.rivalspawnchain.pokedex.PokéNavOverlay;
+import com.rivalspawnchain.pokedex.PokeNavOverlay;
 import net.fabricmc.api.ClientModInitializer;
 
 public class RivalSpawnChainClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        // Register S2C receiver
         ClientNetworkHandler.register();
-
-        // Register silent PokéNav HUD
-        PokéNavOverlay.register();
-
-        RivalSpawnChain.LOGGER.info("[RivalSpawnChain] Client systems ready.");
+        PokeNavOverlay.register();
+        RivalSpawnChain.LOGGER.info("[RivalSpawnChain] Client ready.");
     }
 }
